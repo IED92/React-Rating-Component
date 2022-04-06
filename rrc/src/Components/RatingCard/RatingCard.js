dc0694e6534ed9c8a0a7db295a5efb2052f50b03
Ian <55965026+IED92@users.noreply.github.com>
2022-04-05 22:29:08 -0700

    Added font and more general styling, heavy use of !important to be removed later

--- rrc/src/Components/RatingCard/RatingCard.js
+++ rrc/src/Components/RatingCard/RatingCard.js
@@ -4,31 +4,38 @@ import Card from '@mui/material/Card';
 import logo from '../../logo.svg';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import { IconButton } from '@mui/material';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import styled from '@emotion/styled';
 
 const StyledCard = styled(Card)({})
 
+const StyledIconButton = styled(IconButton)({})
+
 export default function RatingCard() {
   return (
     <StyledCard className='ratingCard'>
       <CardContent className='cardContent'>
-      <div className='starButton'>
+      <StyledIconButton className='starButton'>
         <img src={logo} className="starIcon" alt="star" />
-      </div>
+      </StyledIconButton>
         <Typography className="header" variant="h5" color="white" gutterBottom>
           How did we do?
         </Typography>
-        <Typography variant="body2" color="white" component="div">
+        <Typography className='content' variant="p" color="white" component="div">
           Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
         </Typography>
-        <Typography variant="body1" color="white">
-          1 2 3 4 5
-        </Typography>
+        <div className='container'>
+          <StyledIconButton className='ratingIcon'><p>1</p></StyledIconButton>
+          <StyledIconButton className='ratingIcon'><p>2</p></StyledIconButton>
+          <StyledIconButton className='ratingIcon'><p>3</p></StyledIconButton>
+          <StyledIconButton className='ratingIcon'><p>4</p></StyledIconButton>
+          <StyledIconButton className='ratingIcon'><p>5</p></StyledIconButton>
+        </div>
       </CardContent>
       <CardActions className='cardActions'>
-        <Button size="small">Submit</Button>
+        <Button className='submitButton' size="large"><p>Submit</p></Button>
       </CardActions>
     </StyledCard>
   );
